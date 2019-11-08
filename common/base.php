7c807ba4d6144a091398c3ae7fe369fa8788426c
ddhorstman <davidzilla27@gmail.com>
2019-11-08 16:26:32 -0500

    Added handling of invalid subdomains.

--- common/base.php
+++ common/base.php
@@ -15,7 +15,8 @@
         $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
         $db = new PDO($dsn, DB_USER, DB_PASS);
     } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
+        include_once "splash_screen.php";
+        //echo 'Connection failed: ' . $e->getMessage();
         exit;
     }
 ?>
