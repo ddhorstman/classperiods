b675376724d5a5ad1ddd0a2fccaba0dd5ea2ebd3
ddhorstman <davidzilla27@gmail.com>
2019-11-09 19:08:27 -0500

    Testing participating schools database.
    Testing participating schools database.
    
    Testing participating schools database.

--- welcome.php
+++ welcome.php
@@ -1,5 +1,29 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+ini_set('default_charset','utf-8');
+
+// Start a PHP session
+session_start();
+$_SESSION['date_offset']=0;
+// Include site constants
+echo "Testing";
+include_once "common/inc/constants.inc.php";
+
+// Create a database object
+try {
+    $dsn = "mysql:host=".DB_HOST.";dbname="."participating-schools-info";
+    $db = new PDO($dsn, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+    echo "database error";
+    //echo '<meta http-equiv="refresh" content="0;URL=https://www.classperiods.com/welcome.php">';
+    //echo '</head><body></body></html>';
+    //echo 'Connection failed: ' . $e->getMessage();
+    exit();
+}
+
+
 ?>
 
 Splash screen in development!<br>
