e63a9c2c2ccd1eb951de47c642e87d1d15ee5cca
ddhorstman <davidzilla27@gmail.com>
2019-11-10 16:08:21 -0500

    Added example images.

--- datePicker.php
+++ datePicker.php
@@ -1,12 +1,9 @@
-
 <!--    http://amsul.github.io/pickadate.js -->
     <link rel="stylesheet" href="css/datePicker/default.css">
     <link rel="stylesheet" href="css/datePicker/default.date.css">
-
 <!--         <form>
             <fieldset> -->
                 
-
               
                 <input 
                     style="float: left;"
@@ -17,31 +14,18 @@
                     autofocuss
                     value= "<?php echo date('j F, Y', strtotime($date)) ?>"
                     >
-
                 <!-- <?php echo "The time is " . date("h:i:sa"); ?> -->
                 
-
               
 <!--             </fieldset>
         </form> -->
-
       
         <div id="container"></div>
-
-
-
-
-
-
-
-
   <script src= "js/jquery.1.7.0.js"></script>
    <script src = "js/datePicker/picker.js"></script>
    <script src = "js/datePicker/picker.date.js"></script>
    <script src = "js/datePicker/legacy.js"></script>
-
     <script type="text/javascript">
-
         var $input = $( '.datepicker' ).pickadate({
             // format: 'mm dd yyyy',
             formatSubmit: 'yyyy/mm/dd',
@@ -54,18 +38,10 @@
         })
         //test data
         // for(var i=0;i<validDates.length;i++){
-
         //   console.log(validDates[i])
         // }
-
-
-
         var picker = $input.pickadate('picker')
-
         var lastDatePicked=new Date(picker.get().select);
-
-
-
         function setValidDates(){
           picker = $input.pickadate('picker')
           picker.on({
@@ -85,12 +61,8 @@
           },
           set: function(thingSet) {
             console.log('Just set')
-
-
             
-
             getInfoForNewDate(thingSet.select);
-
             if(thingSet.select){//acctually clicked on a date
               //console.log(dateSelected.getMonth());
               picker.close();
@@ -101,11 +73,9 @@
             // $dateviewing = thingSet/1000; //add one day
             // $date= date('Y-m-d', strtotime($date));
             // document.getElementById("datelable").innerHTML=$date;
-
             //
           }
         })
-
           
            for(var i=0;i<validDates.length;i++){
               var day=new Date(validDates[i].replace(/-/g, '\/'));
@@ -128,8 +98,5 @@
           }
           return false;
         }
-
-
         
-
     </script>
